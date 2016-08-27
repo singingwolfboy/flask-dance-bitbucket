12f3431df11926ce05482b753f7d68f31755cdf6
David Baumgold <david@davidbaumgold.com>
2016-08-27 15:25:56 -0400

    Updates for bitbucket

--- bitbucket.py
+++ bitbucket.py
@@ -12,22 +12,25 @@ app.config["BITBUCKET_OAUTH_CLIENT_ID"] = os.environ.get("BITBUCKET_OAUTH_CLIENT
 app.config["BITBUCKET_OAUTH_CLIENT_SECRET"] = os.environ.get("BITBUCKET_OAUTH_CLIENT_SECRET")
 bitbucket_bp = OAuth2ConsumerBlueprint(
     "bitbucket", __name__,
-    base_url="https://api.bitbucket.org/2.0",
+    base_url="https://api.bitbucket.org/2.0/",
     authorization_url="https://bitbucket.org/site/oauth2/authorize",
     token_url="https://bitbucket.org/site/oauth2/access_token",
+    auto_refresh_url="https://bitbucket.org/site/oauth2/access_token",
 )
 bitbucket_bp.from_config["client_id"] = "BITBUCKET_OAUTH_CLIENT_ID"
 bitbucket_bp.from_config["client_secret"] = "BITBUCKET_OAUTH_CLIENT_SECRET"
-bitbucket = bitbucket_bp.session
 app.register_blueprint(bitbucket_bp, url_prefix="/login")
 
 @app.route("/")
 def index():
+    bitbucket = bitbucket_bp.session
     if not bitbucket.authorized:
         return redirect(url_for("bitbucket.login"))
     resp = bitbucket.get("user")
-    assert resp.ok
-    return "You are @{login} on BitBucket".format(login=resp.json()["login"])
+    assert resp.ok, resp.text
+    return "You are @{username} on Bitbucket".format(
+        username=resp.json()["username"]
+    )
 
 if __name__ == "__main__":
     app.run()
